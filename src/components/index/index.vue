a16ad403830766d8bfd768e956957ee80a99bf40
yangchun <37919689+yangchunaijtt@users.noreply.github.com>
2019-10-18 17:01:40 +0800

    Update index.vue

--- src/components/index/index.vue
+++ src/components/index/index.vue
@@ -130,7 +130,55 @@ export default {
     },
     // 定位效果，定位功能
     chartBus_amap(){
-      
+        let getLocation = (onComplete=function(){},onError=function(){})=>{
+          var map = new AMap.Map('aMap', {
+              resizeEnable: true,
+              zoom: 16
+            }),
+            geolocation = null,
+            geocoder = null
+
+          // 加载地理位置编码插件
+          AMap.service('AMap.Geocoder', function() { //回调函数
+            //实例化Geocoder
+            geocoder = new AMap.Geocoder({
+              city: "010" //城市，默认：“全国”
+            });
+            //TODO: 使用geocoder 对象完成相关功能
+          });
+          // 加载定位插件
+          map.plugin('AMap.Geolocation', function() {
+            // 初始化定位插件
+            geolocation = new AMap.Geolocation({
+              enableHighAccuracy: true, //是否使用高精度定位，默认:true
+              timeout: 10000, //超过10秒后停止定位，默认：无穷大
+              maximumAge: 0, //定位结果缓存0毫秒，默认：0
+              convert: true, //自动偏移坐标，偏移后的坐标为高德坐标，默认：true
+              showButton: true, //显示定位按钮，默认：true
+              buttonPosition: 'LB', //定位按钮停靠位置，默认：'LB'，左下角
+              buttonOffset: new AMap.Pixel(10, 20), //定位按钮与设置的停靠位置的偏移量，默认：Pixel(10, 20)
+              showMarker: false, //定位成功后在定位到的位置显示点标记，默认：true
+              showCircle: false, //定位成功后用圆圈表示定位精度范围，默认：true
+              panToLocation: true, //定位成功后将定位到的位置作为地图中心点，默认：true
+              zoomToAccuracy: true //定位成功后调整地图视野范围使定位位置及精度范围视野内可见，默认：false
+            });
+            // 把定位插件加入地图实例
+            map.addControl(geolocation);
+
+            // 添加地图全局定位事件
+            AMap.event.addListener(geolocation, 'complete', function(date){
+              console.log("成功",data);
+            }); //返回定位信息
+            AMap.event.addListener(geolocation, 'error', function(err){
+              console.log(err);
+            }); //返回定位出错信息
+
+            // 调用定位
+            geolocation.getCurrentPosition();
+          });
+        }
+      // getLocation();
+
     },
   },
   components:{
